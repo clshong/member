8fb5a81df5f3fc458903dc68930ead4edf752781
charles <qcsjin8@gmail.com>
2022-02-06 21:52:25 +0800

    '项目完结'

--- js/list.js
+++ js/list.js
@@ -1,29 +1,29 @@
 // 1.会员列表
-function loadList(){
-  axios({
-  method:'GET',
-  url:'/member-list-page'
-  })
-  .then( res => {
-      const {code,data} = res.data
-      if(code === 200){
-        // 模板引擎渲染数据
-        let html = template('tmp',{
-          members:data,
-        })
-        // 把渲染的数据呈现在页面上
-        document.querySelector('#members').innerHTML = html
-      }else{
-        alert('请求数据失败')
-      }
-  })
-  .catch( err =>{
-    console.log(err);
-  })
-}
+// function loadList(){
+//   axios({
+//     method:'GET',
+//     url:'/member-list-page'
+//     })
+//     .then( res => {
+//         const {code,data} = res.data
+//         if(code === 200){
+//           // 模板引擎渲染数据
+//           let html = template('tmp',{
+//             members:data,
+//           })
+//           // 把渲染的数据呈现在页面上
+//           document.querySelector('#members').innerHTML = html
+//         }else{
+//           alert('请求数据失败')
+//         }
+//     })
+//     .catch( err =>{
+//       console.log(err);
+//     })
+// }
 
-loadList()
-
+// loadList()
+let last = null
 // 2.删除会员
 let members = document.querySelector('#members')
 members.onclick = function(e){
@@ -45,8 +45,35 @@ members.onclick = function(e){
     .then(res => {
       const {code,message} = res.data
       if(code === 200){
-        // 删除成功更新列表
-        loadList()
+        // 删除成功,加载第一页数据
+        last = null
+        axios({
+          method:'GET',
+          url:'/member-list-last',
+          params:{
+            last:last
+          }
+          })
+          .then( res => {
+              const {code,data} = res.data
+              if(code === 200){
+                // 模板引擎渲染数据
+                let html = template('tmp',{
+                  members:data,
+                })
+                // 记录下来最后一条数据ID
+                last = data[data.length - 1 ].id
+                // 把渲染的数据呈现在页面上
+                document.querySelector('#members').innerHTML = html
+              }else{
+                alert('请求数据失败')
+              }
+              // 执行完毕
+              loading =false
+          })
+          .catch( err =>{
+            console.log(err);
+          })
       }else{
         // 删除失败的原因
         alert(message)
@@ -57,3 +84,52 @@ members.onclick = function(e){
     })
   }
 }
+
+//根据最后一条数据获取会员列表
+
+window.onscroll = function(){
+      // 文档高度
+      let docHeight = document.documentElement.scrollHeight;
+      // 浏览器可视区域高度
+      let winHeight = document.documentElement.clientHeight;
+      // 文档滚出去的距离
+      let offset = document.documentElement.scrollTop;
+      if(docHeight - winHeight - offset >= 100) return
+      // 文档在浏览器外面距离如果不到 100px 则去加载下一页数据
+      // 正在加载就不调用loadList
+  if(!loading){
+    loadList()
+  }
+}
+function loadList(){
+  // 正在执行
+  loading = true
+  axios({
+    method:'GET',
+    url:'/member-list-last',
+    params:{
+      last:last
+    }
+    })
+    .then( res => {
+        const {code,data} = res.data
+        if(code === 200){
+          // 模板引擎渲染数据
+          let html = template('tmp',{
+            members:data,
+          })
+          // 记录下来最后一条数据ID
+          last = data[data.length - 1 ].id
+          // 把渲染的数据呈现在页面上
+          document.querySelector('#members').innerHTML += html
+        }else{
+          alert('请求数据失败')
+        }
+        // 执行完毕
+        loading =false
+    })
+    .catch( err =>{
+      console.log(err);
+    })
+}
+loadList()
